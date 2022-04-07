af8f609531899b7e95e7218558bea8efc6d6ed7c
HaEunKI <gkdms3082@naver.com>
2022-04-07 17:38:58 +0900

    프론트엔드프로그래밍2 첫 과제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+function Color({ name, picture }){
+  return (
+    <div>
+      <h2>My favorite color is {name}!</h2>
+      <img src={picture} />
+    </div>
+  );
+}
+
+const colorILike = [
+  {
+    name:'AliceBlue',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.explicit.bing.net%2Fth%3Fid%3DOIP.CE-46BDAfT77qaGSwn2RjwHaDT%26pid%3DApi&f=1',  
+  },
+  {
+    name:'Aqua',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.reDDF7il0CY_TE1Pvk6n1wHaEo%26pid%3DApi&f=1',
+  },
+  {
+    name:'Bisque',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.j5t4WGlcO--hwzfzYY7FrgHaEo%26pid%3DApi&f=1',
+  },
+  {
+    name:'Indigo',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.0yy8ucSz2xWb2PfniXug2wHaEK%26pid%3DApi&f=1',
+  },
+  {
+    name:'HoneyDew',
+    image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.explicit.bing.net%2Fth%3Fid%3DOIP.EjxsWBZ7Lfz98wD_RXDsTwHaE8%26pid%3DApi&f=1'
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {colorILike.map(palette => (
+        <Color name={palette.name} picture={palette.image} />
+      ))}
     </div>
   );
 }
